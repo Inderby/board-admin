4907356572b13fc6a51e9b174bcd07d798b71697
Inderby <youk8253@gmail.com>
2024-03-14 13:09:25 +0900

    #6 - 데이터베이스 접근 로직 테스트 정의
    회원 데이터에 접근할 리포지토리 인터페이스와 jpa auditing 기능을 활성화 시킬 `JpaConfig`, 각자 다른 권한을 가진 테스트용 회원 데이터, crud 쿼리 생성을 확인할 jpa 테스트 작성

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into user_account (user_id, user_password, role_types, nickname, email, memo, created_at, created_by,
+                          modified_at, modified_by)
+values ('inderby', '{noop}asdf1234', 'ADMIN', 'Inderby', 'inderby@mail.com', 'I am Inderby.', now(), 'inderby', now(), 'inderby'),
+       ('mark', '{noop}asdf1234', 'MANAGER', 'Mark', 'mark@mail.com', 'I am Mark.', now(), 'inderby', now(), 'inderby'),
+       ('susan', '{noop}asdf1234', 'MANAGER,DEVELOPER', 'Susan', 'Susan@mail.com', 'I am Susan.', now(), 'inderby', now(),
+        'inderby'),
+       ('jim', '{noop}asdf1234', 'USER', 'Jim', 'jim@mail.com', 'I am Jim.', now(), 'inderby', now(), 'inderby')
+;
