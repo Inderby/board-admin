c9eed333fc1f0a04d4ba6bfb88e0da8267dc5f56
Inderby <youk8253@gmail.com>
2024-03-19 22:18:36 +0900

    #24 - 비즈니스 로직 테스트, 컨트롤러 테스트 작성
    게시글 목록, 단일 게시글, 게시글 삭제 기능을 만들 예정이므로 이들의 테스트를 작성함.

---
+++ src/test/java/com/example/boardadmin/dto/ArticleClientResponse.java
@@ -0,0 +1,30 @@
+package com.example.boardadmin.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.List;
+
+public record ArticleClientResponse(
+        @JsonProperty("_embedded") Embedded embedded,
+        @JsonProperty("page") Page page
+) {
+
+    public static ArticleClientResponse empty(){
+        return new ArticleClientResponse(new Embedded(List.of()), new Page(1, 0,1, 0));
+    }
+
+    public static ArticleClientResponse of(List<ArticleDto> articles) {
+        return new ArticleClientResponse(
+                new Embedded(articles),
+                new Page(articles.size(), articles.size(), 1, 0)
+        );
+    }
+    public List<ArticleDto> articles(){return this.embedded.articles();}
+    record Embedded(@JsonProperty("articles")List<ArticleDto> articles){}
+    record Page(
+       int size,
+       int totalElements,
+       int totalPages,
+       int number
+    ){}
+}
