e73a846aad4b7bde5e144a1a0764855336799b9b
Inderby <youk8253@gmail.com>
2024-03-31 01:17:08 +0900

    #53 - AdminLTE 컴포넌트 + Spring WebSocket 예제를 활용한 채팅 기능 구현
    그럴싸한 채팅 컴포넌트에 스프링 웹소켓 예제를 붙여서
    간단한 유사 채팅 기능을 구현하고 관찰함

---
+++ src/main/java/com/example/boardadmin/controller/WebSocketController.java
@@ -0,0 +1,19 @@
+package com.example.boardadmin.controller;
+
+import com.example.boardadmin.dto.websocket.WebSocketMessage;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.security.core.parameters.P;
+import org.springframework.stereotype.Controller;
+
+import java.security.Principal;
+
+@Controller
+public class WebSocketController {
+
+    @MessageMapping("/hello")
+    @SendTo("/topic/chat")
+    public WebSocketMessage chat(WebSocketMessage message, Principal principal){
+         return WebSocketMessage.of("안녕하세요" + principal.getName() + "!" + message.content() + "라고 하셨나요?");
+    }
+}
